239975663c60ce89096cf47184a3e5782250e088
dexter <80349411+Dexter24601@users.noreply.github.com>
2022-12-28 19:21:22 +0300

    Add files via upload

---
+++ Hadir/Hadir/urls.py
@@ -0,0 +1,77 @@
+"""Hadir URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from HadirApp import views  # . = current folder
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    # http://localhost:8000/admin/
+
+    path('Hadir/', views.index, name="index"),
+    # http://localhost:8000/Hadir/
+
+    path('Hadir/detail', views.detail, name="detail"),
+    # http://localhost:8000/Hadir/detail
+
+    # path('Hadir/student_enrollment',
+    #      views.student_enrollment, name="student_enrollment"),
+    # # http://localhost:8000/Hadir/student_enrollment
+
+
+    path('Hadir/student_enrollment/<str:class_name>-<int:class_id>',
+         views.student_enrollment, name="student_enrollment"),
+    # http://localhost:8000/Hadir/student_enrollment
+
+    path('Hadir/Create_class', views.create_class, name="create_class"),
+    # http://localhost:8000/Hadir/detail
+
+
+    path('Hadir/Classes', views.Classes, name="Classes"),
+    # http://localhost:8000/Hadir/detail
+
+
+    path('Hadir/Classes/<str:class_name>-<int:class_id>', views.clas, name="clas"),
+    # http://localhost:8000/Hadir/detail
+
+
+    path('Hadir/<int:student_id>/absent', views.absent, name="absent"),
+    # http://localhost:8000/Hadir/--
+
+    path('Hadir/images', views.images, name='images'),
+    # http://localhost:8000/Hadir/images
+
+    path('Hadir/register', views.register, name="register"),
+    # http://localhost:8000/Hadir/register
+
+
+    path('Hadir/login', views.login, name="login"),
+    # http://localhost:8000/Hadir/login
+
+    path('Hadir/main', views.mainPage, name="mainPage"),
+
+    path('Hadir/<str:username>/welcomeBack',
+         views.welcomeBack, name="welcomeBack"),
+
+
+    path('Hadir/404', views.PageNotFound, name="PageNotFound")
+    # http://localhost:8000/Hadir/404
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# X:\TU\Capstone project\Engine\Django\Project\Hadir\HadirApp\views.py
+# X:\TU\Capstone project\Engine\Django\Project\Hadir\Hadir\urls.py
